7540d4f69e29f69cff11f8c993ff35f0c4813d6b
ElManu3le <62712661+ElManu3le@users.noreply.github.com>
2020-12-09 19:06:39 +0100

    connectionDataBaseTry1

--- EercicoPracticaAD/src/main/java/Practica1/App.java
+++ EercicoPracticaAD/src/main/java/Practica1/App.java
@@ -1,13 +1,34 @@
 package Practica1;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.*;
+
+public class App {
+    //Enlace para hacer: https://gist.githubusercontent.com/josejuansanchez/c408725e848afd64dd9a20ab37fba8c9/raw/94f317604fda43e5dc7b5e859be82307c62c4488/jardineria.sql
+
+    public static void main(String[] args) {
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "admin", "4DM1n4DM1n");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("Select * from cast");
+
+            while (rs.next()) {
+                String id = rs.getString("id");
+                System.out.println(id);
+                String name = rs.getString("name");
+                System.out.println(name);
+
+            }
+
+            conn.close();
+        } catch (SQLException ex) {
+            
+            ex.printStackTrace();
+        }
+
     }
+
 }
