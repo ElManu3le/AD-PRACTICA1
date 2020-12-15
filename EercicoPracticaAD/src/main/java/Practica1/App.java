b50c9792931a390c3838b4c2bf856606996ad6c7
ElManu3le <62712661+ElManu3le@users.noreply.github.com>
2020-12-15 16:14:35 +0100

    conexion con la base de datos

--- EercicoPracticaAD/src/main/java/Practica1/App.java
+++ EercicoPracticaAD/src/main/java/Practica1/App.java
@@ -3,23 +3,22 @@ package Practica1;
 import java.sql.*;
 
 public class App {
-    //Enlace para hacer: https://gist.githubusercontent.com/josejuansanchez/c408725e848afd64dd9a20ab37fba8c9/raw/94f317604fda43e5dc7b5e859be82307c62c4488/jardineria.sql
 
     public static void main(String[] args) {
 
         Connection conn = null;
 
         try {
-            //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Jardineria", "admin", "4DM1n4DM1n");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jardineria?", "admin", "4DM1n4DM1n");
+            
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jardineria?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "admin", "4DM1n4DM1n");
 
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("Select * from cast");
+            ResultSet rs = stmt.executeQuery("Select * from cliente"); 
 
             while (rs.next()) {
-                String id = rs.getString("id");
-                System.out.println(id);
-                String name = rs.getString("name");
+                String id = rs.getString("codigo_cliente");
+                System.out.print(id);
+                String name = rs.getString("nombre_cliente");
                 System.out.println(name);
 
             }
